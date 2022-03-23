1134bc56b00365556f893a4327f12465006aa42a
Raphael Garnica <garnica.raphael@gmail.com>
2022-03-22 21:21:03 -0300

    ajusta algoritmo 1 para classificacao

--- airflow/dags/etl_scripts/processing_algo1.py
+++ airflow/dags/etl_scripts/processing_algo1.py
@@ -1,15 +1,13 @@
-import os
 import warnings
-import sys
 
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
 from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
+from sklearn.neighbors import KNeighborsClassifier
 
 import logging
 import argparse
@@ -52,12 +50,12 @@ with mlflow.start_run(experiment_id=idExperiment):
     mlflow.log_param("alpha", alpha)
     mlflow.log_param("l1_ratio", l1_ratio)
 
-    regressor = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-    regressor.fit(train_x, train_y)
+    classifier = KNeighborsClassifier()
+    classifier.fit(train_x, train_y)
 
-    predicted_qualities = regressor.predict(test_x)
+    predictions = classifier.predict(test_x)
 
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    (rmse, mae, r2) = eval_metrics(test_y, predictions)
 
     mlflow.log_metric("rmse", rmse)
     mlflow.log_metric("r2", r2)
@@ -65,9 +63,9 @@ with mlflow.start_run(experiment_id=idExperiment):
 
     tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
     if tracking_url_type_store != "file":
-        mlflow.sklearn.log_model(regressor, "model", registered_model_name="IrisModel")
+        mlflow.sklearn.log_model(classifier, "model", registered_model_name="IrisModel")
     else:
-        mlflow.sklearn.log_model(regressor, "model")
+        mlflow.sklearn.log_model(classifier, "model")
 
 
 
